a3f2d17d62b4bdc70e7bf2c1d5cc03a63c268965
DaRuudii <social@daruudii.com>
2019-11-23 00:47:40 +0100

    updated deployment_status.py
    + added timestamp information

--- deployment/deployment_status.py
+++ deployment/deployment_status.py
@@ -5,10 +5,12 @@ environment. This script has to be executed on the console of said environment.
 """
 
 import glob
+import os
+import time
 from pprint import pprint
 
 __author__ = "DaRuudii"
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 __maintainer__ = "DaRuudii"
 __email__ = "social@daruudii.com"
 
@@ -18,9 +20,11 @@ deployment_status = {}
 
 for fname in filenames:
     ip = fname.split(".", 1)[1]
+    timestamp = time.strftime("%Y-%m-%d %H:%M:%S",
+                              time.localtime(os.path.getmtime(fname)))
     with open(fname, 'r') as f:
         status = f.read().strip()
 
-    deployment_status[ip] = status
+    deployment_status[ip] = {"status": status, "timestamp": timestamp}
 
 pprint(deployment_status)
